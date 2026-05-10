f7902b68c16c85133ed42b39723d9bd426e6db9a
dapid16 <muhammaddavid025@gmail.com>
2026-05-10 22:14:11 +0700

    fix utils

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,2 @@
 // GANTI dengan URL publik Cloud Run hasil deploy backend lu
-export const API_URL = "https://backendnotes-dapid-805193520.us-central1.run.app/";
+export const API_URL = "https://backendnotes-dapid-805193520.us-central1.run.app";
