480190d9e3d6cb762330a12bb7563ea912f03a61
dapid16 <muhammaddavid025@gmail.com>
2026-05-10 20:48:41 +0700

    update code app.yaml

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,2 @@
 // GANTI dengan URL publik Cloud Run hasil deploy backend lu
-export const API_URL = "http://localhost:5000/notes";
+export const API_URL = "https://backendnotes-dapid-805193520.us-central1.run.app";
